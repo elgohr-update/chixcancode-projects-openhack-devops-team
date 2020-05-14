c01e22d8c31b0fa65c8c4d76d83b2589e0602419
islamtg <65294740+islamtg@users.noreply.github.com>
2020-05-14 16:38:09 -0400

    ps script to validate

---
+++ .github/workflows/UrlValidation.ps1
@@ -0,0 +1,16 @@
+Param(
+    [string] [Parameter(Mandatory=$true)] $Uri,
+    [boolean] [Parameter(Mandatory=$false)] $displayUri
+    )
+
+$R = Invoke-WebRequest -URI $Uri
+$timestamp = Get-Date
+$output = ""
+if ($displayUri) {
+  $output = $output = '{0} | {1} | {2}' -f($timestamp, $R.StatusCode, $Uri)
+} else {
+
+  $output = '{0} | {1}' -f($timestamp, $R.StatusCode)
+}
+Write-Output $output
+Start-Sleep -Seconds 1
